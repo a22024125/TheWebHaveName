dd9f7bf48f901646a9071bc504d3d3b5a74a4ebd
DESKTOP-F2RQ5U8\Ching <a22024125@gmail.com>
2022-12-27 00:06:11 +0800

    feat:一半WIM上傳照片功能

--- javascript/WIM.js
+++ javascript/WIM.js
@@ -2,24 +2,27 @@ let userName=document.querySelector(".name");
 let birthday=document.querySelector(".date");
 let selfIntro=document.querySelector(".selfIntroduce");
 let subBtn=document.querySelector(".subBtn");
+let upload_pic_btn = document.getElementById("upload_pic_btn");
 
 subBtn.addEventListener("click",e=>{
     e.preventDefault();
     let gender=document.querySelector('input[name="gender"]:checked').value;
 
 
-    // upload_pic_btn = document.getElementById("upload_pic_btn");
-    // //讓user上傳照片
-    // //Save The Data URL String in localStorage
-    // upload_pic_btn.addEventListener("change", (event) => {
-    //     const ig = event.target.files[0];
-    //     const reader = new FileReader();
-    //     reader.readAsDataURL(ig);
-    //     reader.addEventListener("load", ()=> {
-    //         localStorage.setItem("bigPic", reader.result);
-    //         showPic();
-    //     });
-    // })
+    
+    //讓user上傳照片
+    //Save The Data URL String in localStorage
+    upload_pic_btn.addEventListener("change", (event) => {
+        const ig = event.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(ig);
+        localStorage.setItem("bigPic", reader.result);
+        console.log(reader.result);
+        reader.addEventListener("load", ()=> {
+            localStorage.setItem("bigPic", reader.result);
+            showPic();
+        });
+    })
 
 
     localStorage.setItem("name",userName.value);
